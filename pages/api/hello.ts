cb79546182cf006859ef748a138aafddedda0950
TeamOpus <srvopus@gmail.com>
2025-09-28 12:25:27 +0530

    Update hello.ts

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,5 +1,5 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  res.status(200).json({ name: 'BAKA' });
+  res.status(200).json({ name: 'Raam' });
 }
