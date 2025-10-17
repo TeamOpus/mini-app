9933119e7abd249acea1390a7edbd35c36dadf91
TeamOpus <srvopus@gmail.com>
2025-10-17 10:44:25 +0530

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,6 @@
-{
-  "extends": "next",
-  "rules": {
+module.exports = {
+  extends: "next",
+  rules: {
     "react/no-unescaped-entities": "off",
     "@next/next/no-page-custom-font": "off"
   }
